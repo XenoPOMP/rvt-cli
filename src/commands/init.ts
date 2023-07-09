cd1a950852640feec888bc385d53d31e9884f6ac
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-09 22:26:40 +0300

    add default name scanning

--- src/commands/init.ts
+++ src/commands/init.ts
@@ -1,7 +1,14 @@
 import { Args, Command, Flags, ux } from '@oclif/core';
+import * as path from 'path';
+import { writeFile } from 'fs/promises';
 
 import { allowedInitEntities, InitEntity } from '../types/InitEntity';
 import { ChromeExtManifest } from '../types/ChromeExtManifest';
+import { fileExists } from '../utils/fileExists';
+import { inlinePrefix } from '../utils/inlinePrefix';
+import { createFile } from '../utils/createFile';
+import { colors } from '../utils/colors';
+import * as fs from 'fs';
 
 export default class Init extends Command {
 	static description = 'describe the command here';
@@ -25,6 +32,8 @@ export default class Init extends Command {
 
 		const entityTypeArg: InitEntity = args.entity as InitEntity;
 
+		const PROJECT_DIR = process.cwd();
+
 		switch (entityTypeArg) {
 			case 'chrome-extension': {
 				let manifest: ChromeExtManifest = {
@@ -35,20 +44,82 @@ export default class Init extends Command {
 					default_locale: 'en',
 				};
 
+				/**
+				 * Log to inform user that
+				 * utility started to work.
+				 */
+				this.log(colors.green('Chrome extension generation utility.'));
+
 				/** Prompt data from console. */
 				manifest.name = (
-					await ux.prompt('What is the name of extension?', {
-						default: 'My react app',
+					await ux.prompt('Name', {
+						default: (() => {
+							let name = '';
+
+							fileExists(
+								path.join(
+									PROJECT_DIR,
+									'src/assets',
+									'redux/reducers/appSettings.slice.ts',
+								),
+							)
+								.then(() => {
+									fs.readFile(
+										path.join(
+											PROJECT_DIR,
+											'src/assets',
+											'redux/reducers/appSettings.slice.ts',
+										),
+										{},
+										(err, data) => {
+											if (!err) {
+												name = data
+													.toString()
+													.split(/(appName: '.*')/gi)
+													.filter(item => /appName: '.*'/gi.test(item))[0]
+													.replace(/((^appName: ')|('$))/gi, '');
+											}
+										},
+									);
+								})
+								.catch(() => {});
+
+							return name;
+						})(),
 					})
 				)
 					.replace(/\W/gi, '-')
 					.toLowerCase();
 
-				manifest.description = await ux.prompt(
-					'Write description for your extension',
+				manifest.description = await ux.prompt('Description', {
+					required: false,
+				});
+
+				manifest.version = await ux.prompt('Version', {
+					default: manifest.version,
+					required: false,
+				});
+
+				/** Path to generated manifest file. */
+				const pathToManifest = path.join(
+					PROJECT_DIR,
+					'src/manifests/chrome/manifest.json',
 				);
 
 				console.log(manifest);
+
+				/** Create or update manifest file. */
+				// fileExists(pathToManifest)
+				// 	.then(() =>
+				// 		writeFile(pathToManifest, JSON.stringify(manifest))
+				// 			.then(() => this.log(inlinePrefix(`${pathToManifest}`, 'update')))
+				// 			.catch(err => this.error(err)),
+				// 	)
+				// 	.catch(() =>
+				// 		createFile(pathToManifest, JSON.stringify(manifest))
+				// 			.then(() => this.log(inlinePrefix(`${pathToManifest}`, 'create')))
+				// 			.catch(err => this.error(err)),
+				// 	);
 			}
 		}
 	}
