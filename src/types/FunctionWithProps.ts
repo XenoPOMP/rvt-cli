57369af187449a3a2f1a97e5e0e94c4cc95e4b48
XenoPOMP <101574433+XenoPOMP@users.noreply.github.com>
2023-07-12 19:34:04 +0300

    Delete FunctionWithProps.ts

--- src/types/FunctionWithProps.ts
+++
@@ -1,8 +0,0 @@
-export type FunctionWithProps<Args, Return extends any = any> = (
-  args: Args,
-) => Return;
-
-export type FP<Args, Return extends any = any> = FunctionWithProps<
-  Args,
-  Return
->;
